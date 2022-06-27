276a641178f544dcb1302fc3d0a28870e051e342
Adewale-alt <adebimpehabeeb10@gmail.com>
2022-06-27 12:51:02 +0100

    initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
-from models import Post
-.
+from .models import Post
+from django.urls import reverse,reverse_lazy
+
 from django.views import generic
 
 class  PostListView(generic.ListView):
     model = Post
 
-class  PostCreateView(generic.CreatetView):  
+class  PostCreateView(generic.CreateView):  
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)  
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')  
 
-class  PostDetailView(generic.DetailView):
-    model = Post  
+class  PostDetailView(generic.DetailView):  
     model = Post
-    fields = “__all__”
-    success_url  = reverse_lazy(“blog:all”)      
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')      
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')    
+class PostDeleteView(generic.DeleteView):
+    entries = Post.objects.all()
+    entries.delete()
